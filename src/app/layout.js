7f8d5c99c98ed567918e9a35c2ba4ca1accc9863
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2025-08-19 13:35:50 +0200

    add sass

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Poppins } from "next/font/google";
+import Head from "next/head";
+import "../styles/index.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  weight: ["400", "700"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -19,9 +16,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </Head>
+      <body className={`${poppins.className}`}>{children}</body>
     </html>
   );
 }
